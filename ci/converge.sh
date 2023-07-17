802c9cf3eaab1aef6436fc9a5cc81732e535a1f6
Matt Chun-Lum <matt@questionable.engineering>
2023-07-16 20:17:41 -0700

    ci: output to separate passes

--- ci/converge.sh
+++ ci/converge.sh
@@ -10,9 +10,11 @@ mkdir -p /etc/sudoer.d
 echo "$CREATE_USER ALL=(ALL) NOPASSWD: ALL" > "/etc/sudoers.d/$CREATE_USER"
 
 # from scratch
+echo "--- FIRST PASS ---"
 ansible-playbook -i inventory/test.yml --vault-password-file vaultpass "$ROLE"
 
 # second pass to sanity check that we _appear_ idempotent
+echo "--- SECOND PASS ---"
 ansible-playbook -i inventory/test.yml --vault-password-file vaultpass "$ROLE"
 
 rm vaultpass
