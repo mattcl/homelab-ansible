d676399a3d2a4271b5e0347909131acca9fc7a71
Matt Chun-Lum <matt@questionable.engineering>
2023-07-16 20:13:45 -0700

    ci: make a second pass to check that playbook is idempotent

--- ci/converge.sh
+++ ci/converge.sh
@@ -9,6 +9,10 @@ useradd --create-home --shell /bin/bash --groups sudo --user-group "$CREATE_USER
 mkdir -p /etc/sudoer.d
 echo "$CREATE_USER ALL=(ALL) NOPASSWD: ALL" > "/etc/sudoers.d/$CREATE_USER"
 
+# from scratch
+ansible-playbook -i inventory/test.yml --vault-password-file vaultpass "$ROLE"
+
+# second pass to sanity check that we _appear_ idempotent
 ansible-playbook -i inventory/test.yml --vault-password-file vaultpass "$ROLE"
 
 rm vaultpass
